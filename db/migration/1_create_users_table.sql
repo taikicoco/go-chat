1c517dc6c70cceeef964182fa22594bf1d44f4c9
taikicoco <kawasakitaiki0308@gmail.com>
2024-01-26 19:15:28 +0900

    Remove unnecessary code and configuration files

--- db/migration/1_create_users_table.sql
+++
@@ -1,5 +0,0 @@
--- +goose Up
-CREATE TABLE IF NOT EXISTS users (
-    id uint PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-);
--- db/migration/script/migrate-up.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd ../db/migration
-
-goose mysql "user:password@tcp(mysql)/db?charset=utf8mb4&parseTime=true" up
